032aaa49348d3b3cd9ba1dda50d0a21b4dbbe183
Srhii Shapran <sshaprans@gmail.com>
2025-02-03 18:16:30 +0200

    first commit

---
+++ sites/1bN4mUSUM9VpKNwtNVlzen/chunk-WCA4E3HV.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"just a lil cwat doing cwat things... \u{1F431}",favicon:"https://framerusercontent.com/assets/o5YlL3Jj9OJxnEWQpAEWrXB8F8I.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/0TpSCyTUjx3nOfPQ6vTXZnbi1k.jpg",title:"$CWAT"}}export{t as a};
+//# sourceMappingURL=chunk-WCA4E3HV.mjs.map
